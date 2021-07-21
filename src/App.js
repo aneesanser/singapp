ff0b9d7755f89135bd85a8b56b4b73823cdd4bbb
aneesansar <aneesanser@gmail.com>
2021-07-22 01:37:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Form1 from "./components/Form1";
+import Footer from "./components/Footer";
+import Simple from "./components/Simple";
+import Card1 from "./components/Card1";
+import Section from "./components/Section";
+import Dashbord from "./components/Dashbord";
+import FeaturedListing from "./components/FeaturedListing";
+import FeaturedNighbourhoods from "./components/FeaturedNighbourhoods";
+
+function App() {
+  return (
+    <div>
+      <Dashbord />
+    </div>
+  );
+}
+
+export default App;
